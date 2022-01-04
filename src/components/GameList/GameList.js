858f3d7aec2d958f0a2edac9249747232a270d61
Joseph Kwan <jtkwan91@gmail.com>
2022-01-04 03:04:54 -0500

    moved components to individual folders

--- src/components/GameList/GameList.js
+++ src/components/GameList/GameList.js
@@ -1,6 +1,6 @@
 import React from "react"
 import axios from "axios"
-import Game from "./Game.js"
+import Game from "../Game/Game.js"
 import { Link } from "react-router-dom"
 import "./GameList.scss"
 
