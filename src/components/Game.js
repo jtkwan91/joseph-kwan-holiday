78273024b8d31a22bda025df4cf675e35c9b3947
Joseph Kwan <jtkwan91@gmail.com>
2022-01-04 01:56:34 -0500

    styled main page to fit steam theme

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,12 +5,12 @@ class Game extends React.Component {
   render () {
     const { id, name, rating, price, image_uri } = this.props
     return <div className="game">
-        <img className="game__image" src={image_uri} />
-        <div className="game__container">
-          <p className="game__name">{name}</p>
-          <div className="game__details">
-            <p className="game__rating">{rating}% positive</p>
-            <p className="game__price">
+        <img className="game-image" src={image_uri} />
+        <div className="game-container">
+          <p className="game-name">{name}</p>
+          <div className="game-details">
+            <p className="game-rating">{rating}% positive</p>
+            <p className="game-price">
               {price ? `$${price}` : "FREE"}
             </p>
           </div>
