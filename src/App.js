ca0e2976542123aac0fa6fcc3213c2c2438831b6
Joseph Kwan <jtkwan91@gmail.com>
2022-04-14 15:59:09 -0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.scss'
-import {BrowserRouter, Switch , Route} from "react-router-dom"
+import "./App.scss"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 import GameList from "./components/GameList/GameList"
 import GameDetails from "./components/GameDetails/GameDetails"
 
 function Home() {
-  return <>
-    <GameList category="localcoop" title="Local Co-op"/>
-    <GameList category="localpvp" title="Local PVP"/>
-    <GameList category="onlinecoop" title="Online Co-op"/>
-  </>
+  return (
+    <>
+      <h1 className="header">TOP Steam Games</h1>
+      <div className="lists">
+        <GameList category="localcoop" title="Local Co-op" />
+        <GameList category="localpvp" title="Local PVP" />
+        <GameList category="onlinecoop" title="Online Co-op" />
+      </div>
+    </>
+  )
 }
 
 function App() {
@@ -16,8 +21,8 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/game/:id" component={GameDetails}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/game/:id" component={GameDetails} />
         </Switch>
       </BrowserRouter>
     </div>
